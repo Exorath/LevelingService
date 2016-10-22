15884acb624286bfc1a554452e2febba270359c2
Toon Sevrin <twan123@live.be>
2016-10-22 18:38:00 +0200

    Removed unused code

--- main.go
+++ main.go
@@ -6,19 +6,9 @@ import (
 	"net/http"
 )
 
-
-
-
-
-
-
 func main() {
 	ctx := context.Background()
 	svc := levelingService{}
 
-
 	http.ListenAndServe(":8080", MakeHTTPHandler(ctx, svc))
 }
-
-// ErrEmpty is returned when an input string is empty.
-var ErrEmpty = errors.New("empty string")
